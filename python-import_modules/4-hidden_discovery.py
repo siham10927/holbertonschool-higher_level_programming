dffad240057da64d79ee9dd0b4ff448b76b94dc9
siham10927 <10927@holbertonstudents.com>
2025-05-17 14:41:00 +0000

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
+import hidden_4
 if __name__ == "__main__":
-    a = 1
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    for i in dir(hidden_4):
+        if not i.startswith('__'):
+            print(i)
