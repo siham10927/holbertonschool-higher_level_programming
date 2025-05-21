1c278520fe40f8b8657f48ad866782ef2085675b
siham10927 <10927@holbertonstudents.com>
2025-05-21 11:39:53 +0000

    Who are you

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
+
 if __name__ == "__main__":
-    for i in dir(hidden_4):
-        if not i.startswith('__'):
-            print(i)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
