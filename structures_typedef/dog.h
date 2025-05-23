79f0b8199cb5e4d2bc7a91dde6cea8fe60f4108b
siham10927 <10927@holbertonstudents.com>
2025-05-23 17:19:50 +0000

    Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,14 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
-  * struct dog - Dog attributes
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Description: The attributes of a dog.
-  */
+ * struct dog - dog data
+ * @name: dog name
+ * @age: age in years
+ * @owner: name of guardian
+ *
+ * Description: example dog-themed struct for C project 0x0E.
+ */
 struct dog
 {
 	char *name;
@@ -16,13 +16,14 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
-#endif
+
+#endif /* DOG_H */
