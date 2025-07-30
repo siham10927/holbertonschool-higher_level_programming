b262dba34071c7cebfd6aefe0d97a54b2ca38b2d
siham10927 <10927@holbertonstudents.com>
2025-07-30 12:41:31 +0000

    3-my_safe_filt

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - task 4. Cities by states"""
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-    if len(sys.argv) != 4:
-        print('Use: 0-select_states.py <mysql username> <mysql password>'
-              ' <database name>')
-        sys.exit()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
-    cur = conn.cursor()
-    cur.execute('SELECT cities.id, cities.name, states.name FROM cities '
-                'LEFT JOIN states ON cities.state_id = states.id;')
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
